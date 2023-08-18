7769860fa45ded94f6338672ae0a2f58f469ef4a
sujan khatri <sujansince2059@gmail.com>
2023-08-18 20:28:08 +0545

    added child components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import React, { useState } from "react"
 import "./App.css"
 
 const App = () => {
-  return <h1>🏝️ Far Away 🚎</h1>
+  return (
+    <>
+      <Logo />
+      <Form />
+      <Packinglist />
+      <stats />
+    </>
+  )
 }
+
 const Logo = () => {
+  return (
+    <>
+      <h1>🏝️ Far Away 🚎</h1>
+    </>
+  )
+}
+const Form = () => {
   return (
     <>
       <div className="add-form">
