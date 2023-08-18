951ae9f0f7c0ceb0d8e49b740beb720bf1ba0372
sujan khatri <sujansince2059@gmail.com>
2023-08-18 20:24:44 +0545

    created components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,31 @@ import React, { useState } from "react"
 
 import "./App.css"
 
-function App() {
-  return <h1>hello</h1>
+const App = () => {
+  return <h1>🏝️ Far Away 🚎</h1>
+}
+const Logo = () => {
+  return (
+    <>
+      <div className="add-form">
+        <h3>what do you need for Trip?</h3>
+      </div>
+    </>
+  )
+}
+const Packinglist = () => {
+  return (
+    <>
+      <div className="list">list</div>
+    </>
+  )
+}
+const stats = () => {
+  return (
+    <>
+      <footer>You have 9 items in your list and you have packed 4 items</footer>
+    </>
+  )
 }
 
 export default App
